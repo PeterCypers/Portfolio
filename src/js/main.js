9a151a0864e13036d87088f9e0403b9ea7adf809
PeterCypers <peter-cypers@hotmail.com>
2025-10-05 16:40:10 +0200

    version 1.0 completed

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+function navigateTo(relpath) {
+  window.location.href = relpath;
+}
+
+function init() {
+  const cvBtn = document.getElementById("cv_btn");
+  const projectsBtn = document.getElementById("projects_btn");
+  const linkedInBtn = document.getElementById("linkedin_btn");
+  const personalityTestsBtn = document.getElementById("personalitytests_btn");
+  const CV_PATH = "/src/docs/Peter_Cypers_CV(final).pdf";
+  const PROJECTS_PATH = "https://petercypers.github.io/Collected-Projects/";
+  const LINKEDIN_PATH = "#"; //TODO: add real linkedin link when account is ready
+  const PERSONALITY_TESTS_PATH = "/src/docs/PersoonlijkheidsTests_PeterCypers.pdf";
+
+  cvBtn.onclick = () => { navigateTo(CV_PATH); }
+  projectsBtn.onclick = () => { navigateTo(PROJECTS_PATH); }
+  linkedInBtn.onclick = () => { navigateTo(LINKEDIN_PATH); }
+  personalityTestsBtn.onclick = () => { navigateTo(PERSONALITY_TESTS_PATH); }
+
+}
+
+window.onload = init;
