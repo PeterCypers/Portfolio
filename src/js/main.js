4b2358d1386eae66f5498e380769b24d87a562d5
PeterCypers <peter-cypers@hotmail.com>
2025-10-05 17:08:02 +0200

    fixed paths 2

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,10 @@ function init() {
   const projectsBtn = document.getElementById("projects_btn");
   const linkedInBtn = document.getElementById("linkedin_btn");
   const personalityTestsBtn = document.getElementById("personalitytests_btn");
-  const CV_PATH = "Portfolio/src/docs/Peter_Cypers_CV(final).pdf";
+  const CV_PATH = "./src/docs/Peter_Cypers_CV(final).pdf";
   const PROJECTS_PATH = "https://petercypers.github.io/Collected-Projects/";
   const LINKEDIN_PATH = "#"; //TODO: add real linkedin link when account is ready
-  const PERSONALITY_TESTS_PATH = "Portfolio/src/docs/PersoonlijkheidsTests_PeterCypers.pdf";
+  const PERSONALITY_TESTS_PATH = "./src/docs/PersoonlijkheidsTests_PeterCypers.pdf";
 
   cvBtn.onclick = () => { navigateTo(CV_PATH); }
   projectsBtn.onclick = () => { navigateTo(PROJECTS_PATH); }
